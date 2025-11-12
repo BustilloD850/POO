31c82d3a4ffb3967a049aa9781429ff3a75fd11c
Diego Bustillo <diego_3141240180@utd.edu.mx>
2025-11-12 11:38:27 -0600

    ok

--- P3/proyectos_tkinter/mvc/oo/model/operaciones.py
+++ P3/proyectos_tkinter/mvc/oo/model/operaciones.py
@@ -4,46 +4,36 @@ class Operaciones:
     @staticmethod
     def insertar(numero1,numero2,signo,resultado):
         try:
-            sql="insert into operaciones (numero1,numero2,signo,resultado) values (%s, %s, %s, %s)"
-            val=(numero1, numero2, signo, resultado)
-            cursor.execute(sql,val)
+            cursor.execute("insert into operaciones values(null,now(),%s,%s,%s,%s)"
+            ,(numero1, numero2, signo, resultado)
+            )
             conexion.commit()
             return True
         except:
             return False
         
-    def consultar(usuario_id):
+    @staticmethod
+    def consultar():
         try:
-            sql="select * from notas where usuario_id=%s"
-            val=(usuario_id,)
-            cursor.execute(sql,val)
+            cursor.execute("select * from operaciones")
             return cursor.fetchall()
         except:
             return []
         
-    def actualizar(id, titulo, descripcion):
-        try:
-            cursor.execute("SELECT * FROM notas WHERE id=%s", (id,))
-            nota_existente = cursor.fetchone()
-            if nota_existente:
-                cursor.execute("UPDATE notas SET titulo=%s, descripcion=%s, fecha=NOW() WHERE id=%s",
-                            (titulo, descripcion, id))
-                conexion.commit()
-                return True
-            else:
-                return False
-        except:
+    @staticmethod
+    def actualizar(numero1,numero2,signo,resultado):
+         try:
+              cursor.execute("update operaciones set fecha=now(),numero1=%s,numero2=%s,signo=%s,resultado=%s where id=%s", numero1,numero2,signo,resultado)
+              conexion.commit()
+              return True
+         except:
             return False
 
+    @staticmethod
     def eliminar(id):
         try:
-            cursor.execute("SELECT * FROM notas WHERE id=%s", (id,))
-            nota_existente = cursor.fetchone()
-            if nota_existente:
-                cursor.execute("DELETE FROM notas WHERE id=%s", (id,))
-                conexion.commit()
-                return True
-            else:
-                return False
+            cursor.execute("delete from operaciones where id=%s"(id,))
+            conexion.commit()
+            return True
         except:
             return False
